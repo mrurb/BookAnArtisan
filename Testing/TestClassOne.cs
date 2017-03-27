502981909fac0b125fd874f892e1294e2ffef945
Laurids Andersen <lauridsandersen2013@gmail.com>
2017-03-27 14:49:58 +0200

    test classes added

---
+++ Testing/TestClassOne.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using BLL;
+using WCF;
+using Model;
+
+namespace Testing
+{
+    public class TestClassOne
+    {
+        static BLL.Class1 controller = null;
+    public static void setUpBeforeClass() 
+        {
+            try
+            {
+                controller = new BLL.Class1();
+            } catch
+            {
+                throw new Exception();
+            }
+        }
+
+    public static void tearDownAfterClass()
+        {
+            try
+            {
+                controller = null;
+            } catch
+            {
+                throw new Exception();
+            }
+}
+
+    public void setUp()
+        {
+            try
+            {
+                //do nothing
+            } catch
+            {
+                throw new Exception();
+    }
+}
+    public void tearDown()
+    {
+            try
+            {
+                //do nothing
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        public void TestFail()
+        {
+            try
+            {
+                //do nothing
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+
+        public void TestSucceed()
+        {
+            try
+            {
+                //do nothing
+            }
+            catch
+            {
+                throw new Exception();
+            }
+        }
+}
+}
