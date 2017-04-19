7e8df0f95f9223a58691ce8b71d656d3189a1294
Skyggfisk <mahasi93@hotmail.com>
2017-04-19 12:39:33 +0200

    Done things

---
+++ Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+namespace Model
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
---
+++ WCF/UserService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using Model;
+
+namespace WCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "UserService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select UserService.svc or UserService.svc.cs at the Solution Explorer and start debugging.
+    public class UserService : IUserService
+    {
+        public User CreateUser()
+        {
+            return new User { Email = "kdawkdwk@akdkwa.od"};
+        }
+
+        public Boolean DeleteUser(User user)
+        {
+            return true;
+        }
+
+        public User GetUser(int ID)
+        {
+            return new User { Email = "bollocks@bs.dhw"};
+        }
+
+        public List<User> GetUsers()
+        {
+            List<User> List = new List<User>();
+            List.Add(new User { Email = "fucking@whatever.ev"});
+            List.Add(new User { Email = "kwdakdwka" });
+            return List;
+        }
+
+        public User UpdateUser(User user)
+        {
+            return new User { Email = "adadaw"};
+        }
+    }
+}
