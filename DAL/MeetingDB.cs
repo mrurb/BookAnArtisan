6e8025eb49eee3e91b5d7f5e2d385dda6cce55d6
Laurids Andersen <lauridsandersen2013@gmail.com>
2017-04-26 15:29:30 +0200

    Meeting Creation done for now AA-90

--- DAL/MeetingDB.cs
+++ DAL/MeetingDB.cs
@@ -4,14 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Model;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
 
 namespace DAL
 {
     public class MeetingDB : IDataAccess<Meeting>
     {
+        static string connectionstring = ConfigurationManager.ConnectionStrings["DBCon"].ConnectionString;
+
+        /*
+         * Created (26/04) 
+         * Need to consider concurrency
+         * Exception Handling?
+         * Consider Adding contacts etc by ID? sth like dat
+         * RowsAffected btw sucks? shouldnt throw exception
+         */
         public Meeting Create(Meeting t)
         {
-            return null;
+            string query = "INSERT INTO Meeting(StartTime, EndTime, Title, Description, ContactID, CreatedByID) VALUES(@StartTime, @EndTime, @Title, @Description, @ContactID, @CreatedByID);";
+            SqlParameter[] ArrayOfParams =
+            {
+                new SqlParameter { ParameterName = "@StartTime", SqlValue = t.StartTime, SqlDbType = SqlDbType.DateTime },
+                new SqlParameter { ParameterName = "@EndTime", SqlValue = t.EndTime, SqlDbType = SqlDbType.DateTime },
+                new SqlParameter { ParameterName = "@Title", SqlValue = t.Title, SqlDbType = SqlDbType.NVarChar },
+                new SqlParameter { ParameterName = "@Description", SqlValue = t.Description, SqlDbType = SqlDbType.Text },
+                new SqlParameter { ParameterName = "@ContactID", SqlValue = t.ContactId, SqlDbType = SqlDbType.NVarChar },
+                new SqlParameter { ParameterName = "@CreatedByID", SqlValue = t.CreatedById, SqlDbType = SqlDbType.NVarChar },
+           };
+            
+            using (SqlConnection con = new SqlConnection(connectionstring))
+            {
+                using (SqlCommand sqlcommand = new SqlCommand(query, con))
+                {
+                    try
+                    {
+                        sqlcommand.Parameters.AddRange(ArrayOfParams);
+                        con.Open();
+                        int rowsaffected = sqlcommand.ExecuteNonQuery();
+                        if (rowsaffected < 1)
+                        {
+                            throw new System.Exception("No rows affected. Insert failed - Are you a fag?");
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw new Exception();
+                    }
+                }
+            }
+            return t;
         }
 
         public bool Delete(Meeting t)
