1ef02f87c527658356e612b16a185f5f590a8ea3
Laurids Andersen <lauridsandersen2013@gmail.com>
2017-04-28 12:46:49 +0200

    AA-98

--- Model/Meeting.cs
+++ Model/Meeting.cs
@@ -19,7 +19,7 @@ namespace Model
         [DataMember]
         public DateTime EndTime { get; set; }
         [DataMember]
-        public int Id { get; set; }
+        public string Id { get; set; }
         [DataMember]
         public string CreatedById { get; set; }
         [DataMember]
