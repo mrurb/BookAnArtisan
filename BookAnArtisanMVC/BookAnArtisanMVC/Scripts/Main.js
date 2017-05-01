4d7094615fa684a3ca06bdde4ee624700a83b827
mrurb <minikaliffen@gmail.com>
2017-05-01 11:11:54 +0200

    Adding users to meeting via ajax working

---
+++ BookAnArtisanMVC/BookAnArtisanMVC/Scripts/Main.js
@@ -0,0 +1,26 @@
+﻿//$(document).ready(function () {
+function autoc(TextInput, HiddenInput, AjaxUrl){
+    TextInput.autocomplete({
+        source: function (request, response) {
+            var datat;
+            $.ajax({
+                url: AjaxUrl,
+                data: { name: request.term },
+                dataType: "json",
+                type: "POST",
+                success: function (data) { 
+                    response($.map(data, function (item) {
+                        return {
+                            label: item.UserName,
+                            id: item.Id
+                        }
+                    }));
+                }
+            });
+        }, select: function (event, ui){
+            TextInput.val(ui.item.label);
+            HiddenInput.val(ui.item.id);
+        }
+    });
+   }
+//});
