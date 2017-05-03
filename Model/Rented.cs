fea944ab77c2c3dd7a2fd7b5346167e3da1b85c2
Laurids Andersen <lauridsandersen2013@gmail.com>
2017-05-03 14:09:35 +0200

    AA-115, AA-116, AA-117, AA-118  HANGMAN _ _ _ _ _ _ used: L

---
+++ Model/Rented.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+namespace Model
+{
+    [ServiceContract]
+    public class Rented
+    {
+
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public DateTime StartTime { get; set; }
+        [DataMember]
+        public DateTime EndTime { get; set; }
+        [DataMember]
+        public string UserId { get; set; }
+        [DataMember]
+        public object Deleted { get; set; }
+    }
+}
