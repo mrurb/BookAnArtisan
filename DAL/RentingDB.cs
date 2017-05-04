25e12e18ac8b87a93767f0bceaab1a43d340dd9b
Laurids Andersen <lauridsandersen2013@gmail.com>
2017-05-04 15:04:42 +0200

    asdasdasd

--- DAL/RentingDB.cs
+++ DAL/RentingDB.cs
@@ -102,7 +102,8 @@ namespace DAL
                                 EndTime = (DateTime)reader["EndTime"],
                                 StartTime = (DateTime)reader["StartTime"],
                                 Deleted = (bool)reader["Deleted"],
-                                UserId = reader["UserID"].ToString()
+                                User = reader["UserID"].ToString(),
+                                item = reader["somestuff"]
                             };
                         }
                     }
@@ -115,7 +116,7 @@ namespace DAL
         {
             List<Booking> materials = new List<Booking>();
 
-            string sql = "SELECT * FROM Rented";
+            string sql = "SELECT * FROM Bookings JOIN Materials_Unique ON Bookings.MaterialID = Materials_Unique.ID";
 
             using (SqlConnection connection = new SqlConnection(connectionstring))
             {
@@ -132,7 +133,7 @@ namespace DAL
                                 EndTime = (DateTime)reader["EndTime"],
                                 StartTime = (DateTime)reader["StartTime"],
                                 Deleted = (bool)reader["Deleted"],
-                                UserId = reader["UserID"].ToString(),
+                                User = reader["UserID"].ToString(),
                                 item = reader["materialID"]
                             });
                         }
@@ -147,7 +148,7 @@ namespace DAL
             string sql = "if not exists(SELECT StartTime, EndTime FROM Booking WHERE (@starttime <= EndTime AND @endtime >= StartTime) AND @starttime < @endtime AND MaterialID = @materialID AND Deleted = 0) BEGIN UPDATE Booking SET StartTime = @starttime, EndTime = @endtime, UserID = @userID, Deleted = @deleted END";
             SqlParameter[] sqlparams =
             {
-                new SqlParameter { ParameterName = "@userID", SqlValue = t.UserId, SqlDbType = SqlDbType.NVarChar },
+                new SqlParameter { ParameterName = "@userID", SqlValue = t.User.Id, SqlDbType = SqlDbType.NVarChar },
                 new SqlParameter { ParameterName = "@starttime", SqlValue = t.StartTime, SqlDbType = SqlDbType.DateTime },
                 new SqlParameter { ParameterName = "@endtime", SqlValue = t.EndTime, SqlDbType = SqlDbType.DateTime },
                 new SqlParameter { ParameterName = "@materialID", SqlValue = t.item.Id, SqlDbType = SqlDbType.Int },
