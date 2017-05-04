20d464650318fab638eee3ada0758439c5438774
mrurb <minikaliffen@gmail.com>
2017-05-04 15:01:06 +0200

    even more stuff fixed

--- DAL/RentingDB.cs
+++ DAL/RentingDB.cs
@@ -102,7 +102,7 @@ namespace DAL
                                 EndTime = (DateTime)reader["EndTime"],
                                 StartTime = (DateTime)reader["StartTime"],
                                 Deleted = (bool)reader["Deleted"],
-                                UserId = reader["UserID"].ToString()
+                                User = new User { Id = reader["UserID"].ToString() }
                             };
                         }
                     }
@@ -132,8 +132,8 @@ namespace DAL
                                 EndTime = (DateTime)reader["EndTime"],
                                 StartTime = (DateTime)reader["StartTime"],
                                 Deleted = (bool)reader["Deleted"],
-                                UserId = reader["UserID"].ToString(),
-                                item = reader["materialID"]
+                                User = new User { Id =  reader["UserID"].ToString() },
+                                item = new Material { Id = (int)reader["materialID"] }
                             });
                         }
                     }
@@ -147,7 +147,7 @@ namespace DAL
             string sql = "if not exists(SELECT StartTime, EndTime FROM Booking WHERE (@starttime <= EndTime AND @endtime >= StartTime) AND @starttime < @endtime AND MaterialID = @materialID AND Deleted = 0) BEGIN UPDATE Booking SET StartTime = @starttime, EndTime = @endtime, UserID = @userID, Deleted = @deleted END";
             SqlParameter[] sqlparams =
             {
-                new SqlParameter { ParameterName = "@userID", SqlValue = t.UserId, SqlDbType = SqlDbType.NVarChar },
+                new SqlParameter { ParameterName = "@userID", SqlValue = t.User.Id, SqlDbType = SqlDbType.NVarChar },
                 new SqlParameter { ParameterName = "@starttime", SqlValue = t.StartTime, SqlDbType = SqlDbType.DateTime },
                 new SqlParameter { ParameterName = "@endtime", SqlValue = t.EndTime, SqlDbType = SqlDbType.DateTime },
                 new SqlParameter { ParameterName = "@materialID", SqlValue = t.item.Id, SqlDbType = SqlDbType.Int },
