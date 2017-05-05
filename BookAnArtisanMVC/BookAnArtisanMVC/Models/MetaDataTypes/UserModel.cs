bdac7e99d3d58e057dc8055255aa1f569bb7344b
mrurb <minikaliffen@gmail.com>
2017-05-05 13:20:17 +0200

    service fix

--- BookAnArtisanMVC/BookAnArtisanMVC/Models/MetaDataTypes/UserModel.cs
+++ BookAnArtisanMVC/BookAnArtisanMVC/Models/MetaDataTypes/UserModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace BookAnArtisanMVC.UserServiceReference
+namespace BookAnArtisanMVC.ServiceReference
 {
     [MetadataType(typeof(UserMetaData))]
     public partial class User
