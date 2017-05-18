f038d80906617bbe6797efa24cda07dc98f23c9e
Skyggfisk <mahasi93@hotmail.com>
2017-05-18 15:32:55 +0200

    akdkawd

--- Testing/TestingDAL/TestBookings.cs
+++ Testing/TestingDAL/TestBookings.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Model;
 using DAL;
@@ -11,13 +10,11 @@ namespace Testing.TestingDAL
 	public class TestBookings
 	{
 		private static User _user1;
-		private static User _user2;
 		private static Material _material;
 		private static Booking _booking;
 		private static UserDB _uDb;
 		private static MaterialDB _mDb;
 		private static RentingDb _bDb;
-		private static RentingService _rSv;
 
 		#region SetUp and TearDowns
 		[ClassInitialize]
@@ -43,9 +40,7 @@ namespace Testing.TestingDAL
 				_uDb = null;
 				_mDb = null;
 				_user1 = null;
-				_user2 = null;
 				_material = null;
-				_rSv = null;
 			}
 			catch
 			{
@@ -61,10 +56,10 @@ namespace Testing.TestingDAL
 				_uDb = new UserDB();
 				_mDb = new MaterialDB();
 				_bDb = new RentingDb();
-				_rSv = new RentingService();
+				new RentingService();
 				_user1 = _uDb.Read(new User { Id = "2083af25-f483-4a02-a62b-71c198147c84" });
-				_user2 = _uDb.Read(new User { Id = "ef32f29e-1afd-4591-b42c-d0b3838fe6bd" });
-				_material = _mDb.Read(new Material {Id = 4});
+				_uDb.Read(new User { Id = "ef32f29e-1afd-4591-b42c-d0b3838fe6bd" });
+				_material = _mDb.Read(new Material { Id = 4 });
 				_booking = new Booking
 				{
 					Id = 95035,
@@ -92,9 +87,7 @@ namespace Testing.TestingDAL
 				_uDb = null;
 				_bDb = null;
 				_mDb = null;
-				_rSv = null;
 				_user1 = null;
-				_user2 = null;
 				_material = null;
 
 			}
@@ -116,12 +109,8 @@ namespace Testing.TestingDAL
 		[TestMethod]
 		public void TestReadBooking()
 		{
-			var booking2 = _bDb.Read(new Booking {Id =1});
-			if (booking2.User.Id == null)
-			{
-				Assert.IsTrue(false); // fail
-			}
-			Assert.IsTrue(true);
+			var booking = _bDb.Read(new Booking { Id = 1 });
+			Assert.IsTrue(booking.User.Id != null);
 		}
 
 		[TestMethod]
@@ -132,6 +121,22 @@ namespace Testing.TestingDAL
 			{
 				Assert.IsTrue(booking.User.Id != null);
 			}
+		}
+
+
+		[TestMethod]
+		public void TestUpdateBooking()
+		{
+			var booking = _bDb.Read(new Booking { Id = 1 });
+			var oldUpdated = booking.Updated;
+			booking.StartTime = Convert.ToDateTime("2016-05-08 02:00:00.000");
+			booking.EndTime = Convert.ToDateTime("2016-05-10 02:00:00.000");
+			booking.Updated = DateTime.UtcNow;
+
+			_bDb.Update(booking); // do the update
+
+			var booking2 = _bDb.Read(new Booking { Id = 1 }); // read updated entry
+			Assert.IsTrue(booking2.Updated != oldUpdated); // is different = success
 		}
 
 
