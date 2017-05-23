0533d98d11e75ac266efefca2c40a027e7e47368
mrurb <minikaliffen@gmail.com>
2017-05-23 13:33:57 +0200

    stufff

--- DAL/BookingDb.cs
+++ DAL/BookingDb.cs
@@ -238,11 +238,13 @@ namespace DAL
 		{
 			//skip(pager.CurrentPage - 1) * pager.PageSize)
 			List<Booking> materials = new List<Booking>();
+			var totalRows = 0;
 
-			string sql = "SELECT booking.updated, booking.ID bookingID, booking.StartTime starttime, booking.Deleted deleted, booking.EndTime endtime, materials.ID materialID, materials.Name materialsname, materials.Description description, materials.Condition condition, users.ID userID, users.Email email, users.PhoneNumber phonenumber, users.UserName username, users.FirstName firstname, users.LastName lastname, users.Address address FROM Bookings booking JOIN Materials_Unique materials ON booking.MaterialID = materials.ID JOIN AspNetUsers users ON booking.UserID = users.Id WHERE booking.deleted = 0 ORDER BY booking.ID OFFSET @page ROWS FETCH NEXT pageSize ROWS ONLY";
+			string sql = "SELECT booking.updated, booking.ID bookingID, booking.StartTime starttime, booking.Deleted deleted, booking.EndTime endtime, materials.ID materialID, materials.Name materialsname, materials.Description description, materials.Condition condition, users.ID userID, users.Email email, users.PhoneNumber phonenumber, users.UserName username, users.FirstName firstname, users.LastName lastname, users.Address address FROM Bookings booking JOIN Materials_Unique materials ON booking.MaterialID = materials.ID JOIN AspNetUsers users ON booking.UserID = users.Id WHERE booking.deleted = 0 ORDER BY booking.ID OFFSET @page ROWS FETCH NEXT @pageSize ROWS ONLY";
+			string sql2 = "SELECT COUNT(*) AS total FROM Bookings";
 			SqlParameter[] sqlparams =
 			{
-				new SqlParameter { ParameterName = "@page", SqlValue = page-1, SqlDbType = SqlDbType.Int },
+				new SqlParameter { ParameterName = "@page", SqlValue = (((page-1)*pageSize)+1), SqlDbType = SqlDbType.Int },
 				new SqlParameter { ParameterName = "@pageSize", SqlValue = pageSize, SqlDbType = SqlDbType.Int }
 			};
 
@@ -286,6 +288,15 @@ namespace DAL
 						});
 					}
 				}
+
+				command.CommandText = sql2;
+				using (SqlDataReader reader = command.ExecuteReader())
+				{
+					if (reader.Read())
+					{
+						totalRows = (int) reader["total"];
+					}
+				}
 				myTrans.Commit();
 			}
 			catch (Exception)
@@ -297,7 +308,7 @@ namespace DAL
 			{
 				con.Close();
 			}
-			Page<Booking> pages = new Page<Booking>(10, materials, page, pageSize);
+			Page<Booking> pages = new Page<Booking>(totalRows, materials, page, pageSize);
 			return pages;
 		}
 	}
