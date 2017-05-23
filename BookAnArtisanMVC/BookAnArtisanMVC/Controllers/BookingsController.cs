e397b960eed5f64cbed0ab8f9aa5352ec439ba24
mrurb <minikaliffen@gmail.com>
2017-05-23 14:06:43 +0200

    asdfgasdfg

--- BookAnArtisanMVC/BookAnArtisanMVC/Controllers/BookingsController.cs
+++ BookAnArtisanMVC/BookAnArtisanMVC/Controllers/BookingsController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.ServiceModel;
 using System.Web;
 using System.Web.Mvc;
-using BookAnArtisanMVC.BLL;
 using BookAnArtisanMVC.Models;
 using BookAnArtisanMVC.ServiceReference;
 
